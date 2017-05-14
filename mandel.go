3954bb425b1a33dd1d8d3232e4c28c036f107624
Henrik Bohnenkamp <henrik@cs.rwth-aachen.de>
2017-05-14 16:46:11 +0200

    basic scafolding stnads, but mandel needs reimpl

--- mandel.go
+++ mandel.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 import "math/cmplx"
-
+import 	"image"
+import 	"image/color"
 
 const infinity = 10000
 const blocks = 2
+
+type imgval struct  {
+	x,y float64
+	v   int
+}
 
 
 func iter(c complex128, bound float64) int {
@@ -19,7 +25,7 @@ func iter(c complex128, bound float64) int {
         return 0
 }
 
-func mandel(x,y complex128, resx, resy float64, job int, kanal chan int)  {
+func mandel(x,y complex128, resx, resy float64, job int, kanal chan int, imgkanal chan imgval)  {
 
         width := real(y) - real(x)
         height := imag(x) - imag(y)
@@ -28,14 +34,29 @@ func mandel(x,y complex128, resx, resy float64, job int, kanal chan int)  {
                 fmt.Printf("choose your numbers fool: width %f height %f \n", width, height)
 		return
         }
-        for stepx := real(x) ; stepx < real(y) ; stepx += resx {
-                for stepy := imag(x) ; stepy > imag(y) ; stepy -= resy {
+	xcount := 0
+	ycount := 0
+        for stepx := real(x) ; stepx < real(y) ; stepx += resx, xcount++ {
+                for stepy := imag(x) ; stepy > imag(y) ; stepy -= resy, ycount++ {
                         c := complex(stepx,stepy)
-			iter(c,10e+6)
+			rounds := iter(c,10e+6)
+			imgkanal <- imgval{xcount, stepy, rounds}
                 }
 
         }
 	kanal <- job
+}
+
+
+func composeimg(img *image.Gray16, kanal chan imgval) {
+	for true {
+		iv := <-kanal
+		gray := uint8(iv.v % 256)
+		x,y  := int(iv.x), int(iv.y)
+		fmt.Printf("composeimg %d %d %d    %f   %f  \n",  x, y, gray, iv.x, iv.y )
+		
+		img.Set(x,y, color.Gray{gray})
+	}
 }
 
 func main() {
@@ -49,6 +70,10 @@ func main() {
 
 	pixx := 1000
 	pixy := 500
+
+	rect := image.Rect( -pixx/2, pixy/2, pixx/2, -pixy/2)
+	img := image.NewGray16(rect)
+	
 	
 	resx := width / float64(pixx)
 	resy := height / float64(pixy)
@@ -57,13 +82,17 @@ func main() {
 	sizey := height / blocks
 	job := 0
 	kanal :=  make(chan int)
+	imgkanal :=  make(chan imgval)
+
+	go composeimg(img, imgkanal)
+
 	for i := 0 ; i < blocks; i++ {
 		dx :=  real(x) + float64(i) * sizex
 		ddx :=  real(x) + float64(i+1) * sizex
 		for j := blocks ; j > 0; j-- {
 			dy :=  imag(y) + float64(j) * sizey
 			ddy :=  imag(y) + float64(j-1) * sizey
-			go mandel(complex(dx, dy), complex(ddx, ddy), resx, resy, job, kanal)
+			go mandel(complex(dx, dy), complex(ddx, ddy), resx, resy, job, kanal, imgkanal)
 			job++
 		}
 	}
